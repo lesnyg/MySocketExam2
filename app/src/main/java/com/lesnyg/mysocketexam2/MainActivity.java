c8fd5875d1ad67c7ec8d3b034875229308368c8a
lesnyg <lsy79@hanmail.net>
2019-11-11 16:08:49 +0900

    send 완성, received 실패

--- app/src/main/java/com/lesnyg/mysocketexam2/MainActivity.java
+++ app/src/main/java/com/lesnyg/mysocketexam2/MainActivity.java
@@ -11,6 +11,7 @@ import android.os.Handler;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
@@ -18,6 +19,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
+import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.UnknownHostException;
 import java.sql.Blob;
@@ -30,85 +32,59 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
     private String html = "";
-    private Socket socket;
+    private static Socket socket;
+    private static ServerSocket serverSocket;
     private BufferedReader mIn;
-    private BufferedWriter mOut;
-    private String ip = "172.30.1.47";
-    private int port = 5000;
+    private PrintWriter mOut;
+    private String ip = "192.168.0.12";
+    private int port = 8000;
     TextView tv_recevied;
+    EditText et_msg;
+    String msg;
+    String return_msg;
 
-    private AsyncTask<String, Void, Void> mTask;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mTask = new MySyncTask().execute();
-        final EditText et_msg = findViewById(R.id.et_msg);
+        et_msg = findViewById(R.id.et_msg);
         tv_recevied = findViewById(R.id.tv_recevied);
-        findViewById(R.id.btn_call).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (et_msg.getText().toString() != null || !et_msg.getText().toString().equals("")) {
-                    PrintWriter out = new PrintWriter(mOut, true);
-                    String return_msg = et_msg.getText().toString();
-                    out.println(return_msg);
+    }
+
+    public void send_text(View v) {
+        msg = et_msg.getText().toString();
+        NetworkTask networkTask = new NetworkTask();
+        networkTask.execute();
+
+        Toast.makeText(this, "Data sent", Toast.LENGTH_SHORT).show();
+    }
+
+
+    public class NetworkTask extends AsyncTask<Void, Void, Void> {
+        @Override
+        protected Void doInBackground(Void... voids) {
+            try {
+                socket = new Socket(ip, port);
+
+                    mOut = new PrintWriter(socket.getOutputStream());
+                    mOut.write(msg);
+                    mOut.flush();
+
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                mOut.close();
+                try {
+                    socket.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
                 }
             }
-        });
-    }
-
-    public class MySyncTask extends AsyncTask<String, Void, Void> {
-
-        protected void onPreExecute() {
-        }
-
-        @Override
-        protected Void doInBackground(String... strings) {
-            if (isCancelled())
-                return null;
-            query();
             return null;
-
         }
-
-        protected void onPostExecute(String result) {
-        }
-
-        protected void onCancelled() {
-            super.onCancelled();
-        }
-
-    }
-
-
-    public void query() {
-        try {
-            socket = new Socket(ip, port);
-            mIn = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-            mOut = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
-            String line;
-            while (true) {
-                line = mIn.readLine();
-                html = line;
-
-            }
-
-
-        } catch (UnknownHostException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-
-        runOnUiThread(new Runnable() {
-            @Override
-            public void run() {
-                tv_recevied.setText(html);
-            }
-        });
     }
 }
 
