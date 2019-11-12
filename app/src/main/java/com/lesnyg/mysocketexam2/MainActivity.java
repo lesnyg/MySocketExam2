0c54530dc2de7b88973688aa0860b3cda2904ba5
lesnyg <lsy79@hanmail.net>
2019-11-12 16:09:43 +0900

    send 완성, received 완성

--- app/src/main/java/com/lesnyg/mysocketexam2/MainActivity.java
+++ app/src/main/java/com/lesnyg/mysocketexam2/MainActivity.java
@@ -1,39 +1,23 @@
 package com.lesnyg.mysocketexam2;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.io.PrintWriter;
-import java.net.ServerSocket;
 import java.net.Socket;
-import java.net.UnknownHostException;
-import java.sql.Blob;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-    private String html = "";
-    private static Socket socket;
-    private static ServerSocket serverSocket;
+    public static final String TAG = "MainActivity";
+    private static Socket mSocket;
     private BufferedReader mIn;
     private PrintWriter mOut;
     private String ip = "192.168.0.12";
@@ -41,8 +25,10 @@ public class MainActivity extends AppCompatActivity {
     TextView tv_recevied;
     EditText et_msg;
     String msg;
-    String return_msg;
-
+//    String return_msg;
+    Handler handler;
+//    private SenderThread mSenderThread;
+//    private ReceiverThread mReceiverThread;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,40 +37,139 @@ public class MainActivity extends AppCompatActivity {
 
         et_msg = findViewById(R.id.et_msg);
         tv_recevied = findViewById(R.id.tv_recevied);
-    }
 
-    public void send_text(View v) {
-        msg = et_msg.getText().toString();
-        NetworkTask networkTask = new NetworkTask();
-        networkTask.execute();
+        findViewById(R.id.btn_call).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                msg = et_msg.getText().toString();
+                RequestThread thread = new RequestThread();
+                thread.start();
 
-        Toast.makeText(this, "Data sent", Toast.LENGTH_SHORT).show();
-    }
-
-
-    public class NetworkTask extends AsyncTask<Void, Void, Void> {
-        @Override
-        protected Void doInBackground(Void... voids) {
-            try {
-                socket = new Socket(ip, port);
-
-                    mOut = new PrintWriter(socket.getOutputStream());
-                    mOut.write(msg);
-                    mOut.flush();
-
-
-            } catch (IOException e) {
-                e.printStackTrace();
-            } finally {
-                mOut.close();
-                try {
-                    socket.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
             }
-            return null;
+        });
+        handler = new Handler();
+    }
+
+    class RequestThread extends Thread{
+        public void run(){
+            request();
         }
     }
-}
 
+    private void request() {
+
+        try {
+
+            mSocket = new Socket(ip, port);
+            appendText("client started : "+ ip+":"+port);
+//            mSenderThread = new SenderThread(mSocket, msg);
+//            mReceiverThread = new ReceiverThread(mSocket);
+//            mSenderThread.start();
+//            mReceiverThread.start();
+            ObjectOutputStream outputStream = new ObjectOutputStream(mSocket.getOutputStream());
+            outputStream.writeUTF("Hello");
+            outputStream.flush();
+
+            appendText("Hello sent");
+
+            ObjectInputStream inputStream = new ObjectInputStream(mSocket.getInputStream());
+            String inStr = inputStream.readUTF();
+
+            appendText("inStr from server : " + inStr);
+            mSocket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    private void appendText(String appmag){
+        final String inMsg = appmag;
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                tv_recevied.append(inMsg+'\n');
+            }
+        });
+
+    }
+
+}
+//
+///**
+// * 메시지를 수신하는 스레드 입니다.
+// */
+//class ReceiverThread extends Thread {
+//    private static final String TAG = "ReceiverThread";
+//    Socket socket;
+//
+//    public ReceiverThread(Socket socket) {
+//        this.socket = socket;
+//    }
+//
+//    @Override
+//    public void run() {
+//
+//        try {
+//            // 서버로부터 인풋스트림이라는 통로를 통해 들어오는 문자열을 잠시 보관하는 객체입니다.
+//            BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+//
+//            while (true) {
+//                String str = reader.readLine();
+//                if (str == null) {
+//                    break;
+//                }
+//                Log.d(TAG, reader.readLine());
+//
+//            }
+//        } catch (Exception e) {
+//            Log.d(TAG, e.getMessage());
+//        }
+//
+//    }
+//
+//}
+//
+///**
+// * 메시지를 발신하는 스레드 입니다.
+// */
+//class SenderThread extends Thread {
+//    Socket socket;
+//
+//    public SenderThread(Socket socket, String message) {
+//        this.socket = socket;
+//    }
+//
+//    @Override
+//    public void run() {
+//
+//        try {
+//
+//            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+//            PrintWriter writer = new PrintWriter(socket.getOutputStream());
+//
+//            while (true) {
+//                String str = reader.readLine();
+//
+//                if (str.equals("bye")) {
+//                    break;
+//                }
+//
+//                // 입력한 문자열을 서버로 발신합니다.
+//                writer.println(str);
+//                writer.flush();
+//            }
+//
+//        } catch (Exception e) {
+//            System.out.println(e.getMessage());
+//        } finally {
+//            try {
+//                socket.close();
+//            } catch (IOException e) {
+//                // TODO Auto-generated catch block
+//                e.printStackTrace();
+//            }
+//        }
+//
+//    }
+//
+//}
+//
